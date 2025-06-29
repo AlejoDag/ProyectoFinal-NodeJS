ff87fcfb4718f2f5ef57cca85e06203041bceb85
AlejoDag <alejodaguanno@gmail.com>
2025-06-29 00:52:22 -0300

    Correccion firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,13 @@
-import admin from "firebase-admin";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+initializeApp({
+  credential: cert(serviceAccount),
 });
 
-const db = admin.firestore();
+const db = getFirestore();
 
 export { db };
