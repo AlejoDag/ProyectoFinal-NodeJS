e613dd49c5d75bdce92b442a650aa8380c7999a6
AlejoDag <alejodaguanno@gmail.com>
2025-07-11 19:23:50 -0300

    Actualización

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const firebaseConfig = {
+const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 initializeApp({
-  credential: cert(firebaseConfig),
+  credential: cert(serviceAccount),
 });
 
 export const db = getFirestore();
